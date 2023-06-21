9309f336ced452fb00bf6c48d9c5e4dfa5412f6c
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2023-06-22 00:59:29 +0800

    delete field

--- database/migrations/2023_06_21_114408_create_quizzes_table.php
+++ database/migrations/2023_06_21_114408_create_quizzes_table.php
@@ -19,8 +19,6 @@ return new class extends Migration
             $table->unsignedBigInteger('course_id');
             $table->enum('test_type', ['pre', 'post']);
             $table->string('title');
-            $table->timestamp('start_time')->nullable();
-            $table->timestamp('end_time')->nullable();
             $table->timestamps();
 
             $table->foreign('course_id')->references('id')->on('courses')
